02ae5dad806639e1cc3be49287942657b4d8dacc
IraTokar <tokar.ira20@gmail.com>
2024-04-05 23:26:29 +0200

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = galleryItemsMarkup(galleryItems);
+
+function galleryItemsMarkup(item) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+                <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a></div>
+                `
+    }).join('');
+};
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const SimpleLightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+    captionPosition: 'bottom',
+    enableKeyboard: true,
+    
+});
